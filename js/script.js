9bb6994407c58aca62b219e903ed5b85ad4543d5
Ricardo Andres Castro Ahumada <ricardocastro.ti@gmail.com>
2020-07-28 19:26:22 -0300

    Atualização de Promise.All

--- js/script.js
+++ js/script.js
@@ -5,23 +5,63 @@ let globalCountries = [];
 let globalUsersCountries = [];
 
 async function start() {
-  await fetchUsers();
-  await fetchCountries();
+  //Normal
+  // await fetchUsers();
+  // await fetchCountries();
+
+  //Promise Sequencial
+  console.time('Promise');
+  await promiseCountries();
+  await promiseUsers();
+  console.timeEnd('Promise');
+
+  //Promise Paralela
+  const p1 = promiseUsers();
+  const p2 = promiseCountries();
+  await Promise.all([p1, p2]);
+
   hidespinners();
   mergeUsersAndCountries();
   render();
 }
+
+function promiseUsers() {
+  return new Promise(async (resolve, reject) => {
+    const users = await fetchUsers();
+
+    setTimeout(() => {
+      console.log('promiseUsers Resolvida!');
+      resolve(users);
+    }, 2000);
+  });
+}
+
+function promiseCountries() {
+  return new Promise(async (resolve, reject) => {
+    const countries = await fetchCountries();
+
+    setTimeout(() => {
+      console.log('promiseCountries Resolvida!');
+      resolve(countries);
+    }, 2000);
+  });
+}
+
 async function fetchUsers() {
   const resources = await fetch('http://localhost:3002/users');
   const dataJson = await resources.json();
-  globalUser = dataJson.map((user) => {
-    return {
-      nameUser: user.name,
-      pictureUser: user.picture,
-      loginUser: user.login,
-      natUser: user.nat,
-    };
-  });
+  globalUser = dataJson
+    .map((user) => {
+      return {
+        nameUser: user.name,
+        pictureUser: user.picture,
+        loginUser: user.login,
+        natUser: user.nat,
+      };
+    })
+    .sort((a, b) => {
+      return a.natUser.localeCompare(b.natUser);
+    });
   // console.log(globalUser);
 }
 
@@ -60,7 +100,7 @@ function render() {
       ${globalUsersCountries
         .map(({ pictureUser, flagCountry, nameUser, nameCountry }) => {
           return `
-                <div class='col-3'>
+                <div class='col-sm-3'>
                   <div class='d-flex flex-row bordered'>
                     <img class='avatar' src='${pictureUser.medium}' alt='${nameUser.first}' />
                     <div class='d-flex flex-column'>
